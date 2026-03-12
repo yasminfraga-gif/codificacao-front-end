17b97e0ccddbc667a43719710e21a00c5bd6f0b8
yasminfraga-gif <yasmin_fraga@estudante.sesisenai.org.br>
2026-03-12 16:08:23 -0300

    Create atividade.js

---
+++ 12-03-2026/atividade.js
@@ -0,0 +1,34 @@
+ 
+// a = 4
+// b = 3
+// console.log(a+b)
+
+
+
+// a = 8
+// b = 3
+// console.log(a-b)
+
+
+
+// a = 3
+// b = 3
+// console.log(a*b)
+
+
+
+// a = 6
+// b = 2
+// console.log(a/b)
+
+
+
+// a = 10
+// b = 4
+// console.log(a%b)
+
+
+
+// a = 2
+// b = 3
+// console.log(a**b)
