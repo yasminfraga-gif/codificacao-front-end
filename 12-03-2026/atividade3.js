995d5c8236766aacb2af0a11758dd4add82e3b5d
yasminfraga-gif <yasmin_fraga@estudante.sesisenai.org.br>
2026-03-12 17:02:43 -0300

    atividade 3

---
+++ 12-03-2026/atividade3.js
@@ -0,0 +1,10 @@
+let idade = 61;
+let preco = 110;
+let precofinal
+
+if (idade > 60 && preco > 100) {
+     precofinal = preco - (preco * 0.20)
+     console.log('desconto aplicado!! novo preço: ' + precofinal)
+} else {
+     console.log('desconto não aplicado, preço final: ')
+}
